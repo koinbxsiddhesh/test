e3298bb9f6f8ad5fc92c3a904d67226838a35461
Siddhesh Shinde <siddhesh@koinbx.com>
2024-09-23 15:47:45 +0530

    fix: trust proxy

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ExpressAdapter } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const adapter = new ExpressAdapter();
+  adapter.set('trust proxy', 1); 
+  const app = await NestFactory.create(AppModule,adapter);
+
   await app.listen(3002);
 }
 bootstrap();
